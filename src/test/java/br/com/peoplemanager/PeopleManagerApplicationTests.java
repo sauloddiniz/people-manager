1e3754d02d41eb5888de284c76d8a266f44ae16a
Saulo Dias Diniz <sauloddiniz@gmail.com>
2024-05-08 11:29:09 -0300

    Initial commit

---
+++ src/test/java/br/com/peoplemanager/PeopleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.peoplemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
