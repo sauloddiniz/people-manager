4b33ff3509f4816ae802a32d49452686f405eaff
Saulo Dias Diniz <sauloddiniz@gmail.com>
2024-05-10 02:33:45 -0300

    Reconfigured Jenkinsfile to include Docker image building
    In the Jenkinsfile, the deployment stage has been replaced with a stage to build a Docker image. This modification allows for a more containerized approach and streamlines the deployment process. A Dockerfile change ensuring properly set entry point complements this update.

--- src/main/java/br/com/peoplemanager/controller/AddressController.java
+++ src/main/java/br/com/peoplemanager/controller/AddressController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/person/{personId}/addresses")
+@CrossOrigin(origins = "*")
 public class AddressController {
 
     private final AddressService addressService;
