119b14fc9e1060f467f8a396d6437f26de330c64
Saulo Dias Diniz <sauloddiniz@gmail.com>
2024-05-12 20:52:01 -0300

    Add personId to PersonDto and its converter
    Added a new attribute 'personId' to the PersonDto class and updated the PersonConverter to convert this field from Person objects. This additional information further exposes details of the Person objects in their data transfer representation.

--- src/main/java/br/com/peoplemanager/dto/person/PersonDto.java
+++ src/main/java/br/com/peoplemanager/dto/person/PersonDto.java
@@ -1,6 +1,7 @@
 package br.com.peoplemanager.dto.person;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.Data;
 public class PersonDto {
     private String personId;
     @NotEmpty
+    @Pattern(regexp="^\\w+\\s+\\w+.*$", message="Full name must contain at least two words.")
     private String fullName;
     @NotEmpty
     private String birthDate;
