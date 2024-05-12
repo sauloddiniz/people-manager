3c4e531b69051cd66b8662a0efd5771d791832e7
Saulo Dias Diniz <sauloddiniz@gmail.com>
2024-05-12 18:47:05 -0300

    Reconfigured Jenkinsfile to include Docker image building
    In the Jenkinsfile, the deployment stage has been replaced with a stage to build a Docker image. This modification allows for a more containerized approach and streamlines the deployment process. A Dockerfile change ensuring properly set entry point complements this update.

--- src/main/java/br/com/peoplemanager/PeopleManagerApplication.java
+++ src/main/java/br/com/peoplemanager/PeopleManagerApplication.java
@@ -2,10 +2,16 @@ package br.com.peoplemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class PeopleManagerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PeopleManagerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PeopleManagerApplication.class, args);
